6df12a9d4ea5ba78d95bea2d13205c97df38f182
Realida <bipanapariyar04@gmail.com>
2026-05-01 13:48:46 +0545

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='Aryan_chatbot',
+    version='0.1.0',  
+    author='Bipana Pariyar',
+    author_email="bipanapariyar04@gmail.com",
+    packages = find_packages(),
+    install_requires=[]
+)
---
+++ template.sh
@@ -0,0 +1,21 @@
+#creating my file structure for my project
+mkdir -p Src
+mkdir -p resources
+
+
+
+
+#creating file
+touch Src/ _init_.py
+touch Src/helper.py
+touch Src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch resources/trials.ipynb
+touch requirements.txt
+
+
+
+
+echo "Project structure created successfully!"
